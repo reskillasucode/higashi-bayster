6440e4f0a070f373b95f6a8db3061be7da962354
root <root@Mega>
2023-05-25 20:50:44 +0900

    Model追加、list.html

---
+++ bayster/land/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+
+from .models import Land
+
+# Create your views here.
+class LandListView(ListView):
+    model = Land
+    template_name = 'land/land_list.html'
+
+class LandDetailView(DetailView):
+    model = Land
+    template_name = 'land/land_detail.html'
+
+class LandCreateView(CreateView):
+    model = Land
+    template_name = 'land/land_create.html'
+    fields = '__all__'
