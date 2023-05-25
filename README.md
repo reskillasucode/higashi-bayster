ca518a148be1d975660996a80b1c1a11335c28e3
root <root@Mega>
2023-05-25 18:13:32 +0900

    ListView作成まで

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # higashi-bayster
+test pushh
--- land/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('land/', views.LandListView.as_view(), name='land_list'),
-    path('land/<int:pk>/', views.LandDetailView.as_view(), name='land_detail'),
-    path('create/', views.LandeCreateView.as_view(), name='land_create'),
-]
